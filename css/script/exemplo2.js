48c527e494962849951c9ce930c5122b22558390
lucileide <leide.contact@gmail.com>
2023-05-16 22:20:54 -0400

    leide

---
+++ css/script/exemplo2.js
@@ -0,0 +1,38 @@
+/*
+var n1 = parseInt(prompt('Informe o primeiro valor:'));
+var n2 = parseInt(prompt('Informe o segundo valor:'));
+
+//parseFloat() para converter para números reais...
+
+//alert(n1+n2);
+
+result.innerHTML = n1+n2;
+
+
+var resp = confirm("Podemos continuar?");
+
+document.write("<p>" + resp + "</p>");
+
+console.log(n1+n2);
+
+var n1 = parseInt(prompt('Informe o primeiro número:'));
+
+var n2 = parseInt(prompt('Informe o segundo número:'));
+
+var n3 = parseInt(prompt('Informe o terceiro número:'));
+
+var n4 = parseInt(prompt('Informe o quarto número'));
+
+alert((n1+n2+n3+n4)/4);
+
+var saldo = parseInt(prompt('Informe o saldo:'));
+
+alert((saldo * 2/100) + saldo);
+*/
+
+var segundos = parseInt(prompt('Informe a quantidade de segundos:'));
+var minutos =segundos  /60; 
+var s = segundos % 60;
+
+alert(segundos +'segundos equivale a' + minutos +' minutos e ' + s + 'segundos')
+
